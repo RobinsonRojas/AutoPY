d8ac14ccf50305db2b78565487b6157ba2801396
Robinson Rojas <totorox123@gmail.com>
2022-02-04 12:11:14 -0500

    Colocando el Driver de wewebdriver_manager como objeto

--- PY.py
+++ PY.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 
+# Dejar esta lib cierra la venta
+from webdriver_manager.chrome import ChromeDriverManager
 
+options = Options()
+options.add_argument("start-maximized")
 
-#from webdriver_manager.chrome import ChromeDriverManager
-
-#driver = webdriver.Chrome(ChromeDriverManager().install())
-driver = webdriver.Chrome('C:\d\chromedriver.exe')
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://www.google.com")
-
-driver.title # => "Google"
-
 driver.implicitly_wait(0.5)
 
 search_box = driver.find_element(By.NAME, "q")
@@ -20,6 +21,9 @@ search_button = driver.find_element(By.NAME, "btnK")
 search_box.send_keys("Selenium")
 search_button.click()
 
-driver.find_element(By.NAME, "q").get_attribute("value") # => "Selenium"
+driver.set_window_size(500,500)
 
-driver.set_window_size(500,500,)
+print("\n ====== Test Results ======")
+print("✅ Tests successfully passed!")
+print(driver.title) # => "title de la web"
+print(driver.find_element(By.NAME, "q").get_attribute("value")) # => "Contenido del elemento en el estado actual"
